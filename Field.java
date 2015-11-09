f17d6859db071219aed72ccfc3bc481156914550
sinbart0 <sinbart0@gmail.com>
2015-11-09 21:07:08 +0300

    Update Field.java

--- Field.java
+++ Field.java
@@ -2,32 +2,62 @@ package com.lesson;
 
 public class Field {
 
-    private static final int FIELD_SIZE = 3;
+    private static final int DEFAULT_FIELD_SIZE = 4;
+
+    private static final int MIN_FIELD_SIZE = 0;
+
+    private static final int MAX_FIELD_SIZE = 100;
 
     private static final char DEFAULT_CELL_VALUE = ' ';
 
-    private char[][] field = new char[DEFAULT_CELL_VALUE][DEFAULT_CELL_VALUE];
+    private int fieldSize;
+
+    private char[][] field;
+
+    public Field() {
+        this(DEFAULT_FIELD_SIZE);
+        System.out.println("In constructor");
+    }
+
+    public Field(int size) {
+        fieldSize = size;
+        field = new char[fieldSize][fieldSize];
+    }
+
+    //private static final int fieldSize = 3;
+
+//    public void setFieldSize(int newFieldSize) {
+//        if (newFieldSize > MIN_FIELD_SIZE && newFieldSize < MAX_FIELD_SIZE) {
+//            fieldSize = newFieldSize;
+//            }
+//    }
+
+    public int getFieldSize() {
+        return fieldSize;
+    }
 
     public void eraseField() {
-        for (int j = 0; j < FIELD_SIZE; j++) {
+        for (int j = 0; j < fieldSize; j++) {
             eraseLine(j);
         }
     }
 
     public void showField() {
-        for (int j = 0; j < FIELD_SIZE; j++) {
+        System.out.println();
+        for (int j = 0; j < fieldSize; j++) {
             showLine(j);
         }
+        System.out.println();
     }
 
     private void eraseLine(int lineNumber) {
-        for (int i = 0; i < FIELD_SIZE; i++) {
+        for (int i = 0; i < fieldSize; i++) {
             field[i][lineNumber] = DEFAULT_CELL_VALUE;
         }
     }
 
     private void showLine(int lineNumber) {
-        for (int i = 0; i < FIELD_SIZE; i++) {
+        for (int i = 0; i < fieldSize; i++) {
             System.out.print("[" + field[i][lineNumber] + "]");
         }
         System.out.println();
