2445b341d94e516dece7ac6191014a5af6524445
sinbart0 <sinbart0@gmail.com>
2015-11-09 19:39:24 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+package com.lesson;
+
+public class Main {
+    public static void main(String[] args) {
+        Field testField = new Field();
+        testField.eraseField();
+        testField.showField();
+    }
+}
